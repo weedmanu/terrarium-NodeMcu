5be25c3c4a493151bd5fef4af9fff28bdcbe1873
Weedmanu <terraspi13@gmail.com>
2016-11-19 15:14:35 +0100

    Update dht22bdd.php

--- Partie_web/terranodemcu/dht22bdd.php
+++ Partie_web/terranodemcu/dht22bdd.php
@@ -11,8 +11,5 @@
 $sqlt = "insert into dht22.temphumi (tempC,humiC,tempF,humiF) values ($tempC,$humiC,$tempF,$humiF)";
 mysql_query($sqlt);
 
-if ($tempC < 20) {	
-	fopen("https://smsapi.free-mobile.fr/sendmsg?user=16211308&pass=bLdaXQbP4f6DRn&msg=Alerte%20!%20Limit%20atteinte%20:%20tempC%20=%20$tempC%20°C%20,%20tempF%20=%20$tempF%20°C%20,%20humiC%20=%20$humiC%20%%20,%20humiF%20=%20$humiF%%20", "r");
-}
 		
 ?>
