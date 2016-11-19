15347ec54b0aa9a40c61cccc31e76562a7151c29
manu <manu@localhost.localdomain>
2016-11-19 15:24:24 +0100

    modifié :         Partie_web/terranodemcu/dht22bdd.php 	modifié :         Partie_web/terranodemcu/ping.py

--- Partie_web/terranodemcu/dht22bdd.php
+++ Partie_web/terranodemcu/dht22bdd.php
@@ -1,18 +1,23 @@
  <?php
  
-   $result = mysql_connect("localhost", "XXXX", "YYYY"); 
+   $result = mysql_connect("localhost", "XXXX", "YYYY"); // connexion à la base de donnée
    
+   // récupération des variable GET html et on les passe en variable php
     $tempC = $_GET["tempC"];
     $humiC = $_GET["humiC"];
     $tempF = $_GET["tempF"];
     $humiF = $_GET["humiF"];
     
-    
+    // on insert les datas dans la base de donnée
 $sqlt = "insert into dht22.temphumi (tempC,humiC,tempF,humiF) values ($tempC,$humiC,$tempF,$humiF)";
 mysql_query($sqlt);
 
+//on ferme la connexion à MySQL
+mysql_close($result);
+
+// envoi d'un sms ( fonctionne uniquement chez free mobile option gratuite)
 if ($tempC < 20) {	
-	fopen("https://smsapi.free-mobile.fr/sendmsg?user=16211308&pass=bLdaXQbP4f6DRn&msg=Alerte%20!%20Limit%20atteinte%20:%20tempC%20=%20$tempC%20°C%20,%20tempF%20=%20$tempF%20°C%20,%20humiC%20=%20$humiC%20%%20,%20humiF%20=%20$humiF%%20", "r");
+	fopen("https://smsapi.free-mobile.fr/sendmsg?user=XXXXXXXX&pass=YYYYYYYYY&msg=Alerte%20!%20Limit%20atteinte%20:%20tempC%20=%20$tempC%20°C%20,%20tempF%20=%20$tempF%20°C%20,%20humiC%20=%20$humiC%20%%20,%20humiF%20=%20$humiF%%20", "r");
 }
 		
 ?>
--- Partie_web/terranodemcu/ping.py
+++ Partie_web/terranodemcu/ping.py
@@ -4,10 +4,11 @@
 import urllib2
 import os
 
-hostname = "192.168.0.30" # terranodemcu
+hostname = "192.168.XXX.XXX" # terranodemcu
 response = os.system("ping -c 1 " + hostname)
 
-req = urllib2.Request("https://smsapi.free-mobile.fr/sendmsg?user=16211308&pass=bLdaXQbP4f6DRn&msg=Alerte%20!%20%20Connexion%20perdu%20!!!")
+# envoi d'un sms ( fonctionne uniquement chez free mobile option gratuite)
+req = urllib2.Request("https://smsapi.free-mobile.fr/sendmsg?user=XXXXXXXX&pass=YYYYYYYYY&msg=Alerte%20!%20%20Connexion%20perdu%20!!!")
 
 # et on controle la reponse
 if response == 0:
@@ -15,4 +16,4 @@ if response == 0:
   print hostname, 'is up!'
 else:
   print hostname, 'is down!'
-  url=urllib2.urlopen(req)
+  url=urllib2.urlopen(req) # on envoi le sms si la com est perdu
