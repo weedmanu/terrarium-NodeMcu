de44137b5c699620f5dd2addf36adb303aa6bf2a
weedmanu <weedmanu@hotmail.com>
2017-02-23 10:44:15 +0100

    modifié :         Partie_web/terranodemcu/datamoyh.php

--- Partie_web/terranodemcu/datamoyh.php
+++ Partie_web/terranodemcu/datamoyh.php
@@ -17,7 +17,7 @@ try {
     $PDO = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
 	
-	$reponse = $PDO->query("SELECT DATE_FORMAT(date,'%d-%m-%Y %H') as moyheure FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL $limit HOUR) GROUP BY DAY(date), HOUR(date)");	
+	$reponse = $PDO->query("SELECT DATE_FORMAT(date,'%d-%m-%Y %H') as moyheure FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL $limit HOUR) GROUP BY MONTH(date), DAY(date), HOUR(date)");	
 	$rows = array();
 	$rows['name'] = 'moyheure';
 	while ($donnees = $reponse->fetch())
@@ -25,7 +25,7 @@ try {
 		$rows['data'][] = $donnees['moyheure'];		
 	}	
 	
-	$reponse = $PDO->query('SELECT ROUND(AVG(TempC),2) as TempC FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY DAY(date), HOUR(date)');	
+	$reponse = $PDO->query('SELECT ROUND(AVG(TempC),2) as TempC FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY MONTH(date), DAY(date), HOUR(date)');	
 	$rows1 = array();
 	$rows1['name'] = 'TempC';
 	while ($donnees = $reponse->fetch())
@@ -33,7 +33,7 @@ try {
 		$rows1['data'][] = $donnees['TempC'];		
 	}
 	
-	$reponse = $PDO->query('SELECT ROUND(AVG(TempF),2) as TempF FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY DAY(date), HOUR(date)');	
+	$reponse = $PDO->query('SELECT ROUND(AVG(TempF),2) as TempF FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY MONTH(date), DAY(date), HOUR(date)');	
 	$rows2 = array();
 	$rows2['name'] = 'TempF';
 	while ($donnees = $reponse->fetch())
@@ -41,7 +41,7 @@ try {
 		$rows2['data'][] = $donnees['TempF'];		
 	}
 	
-	$reponse = $PDO->query('SELECT ROUND(AVG(HumiC),2) as HumiC FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY DAY(date), HOUR(date)');	
+	$reponse = $PDO->query('SELECT ROUND(AVG(HumiC),2) as HumiC FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY MONTH(date), DAY(date), HOUR(date)');	
 	$rows3 = array();
 	$rows3['name'] = 'HumiC';
 	while ($donnees = $reponse->fetch())
@@ -49,7 +49,7 @@ try {
 		$rows3['data'][] = $donnees['HumiC'];		
 	}
 	
-	$reponse = $PDO->query('SELECT ROUND(AVG(HumiF),2) as HumiF FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY DAY(date), HOUR(date)');	
+	$reponse = $PDO->query('SELECT ROUND(AVG(HumiF),2) as HumiF FROM temphumi WHERE date < NOW() AND date > DATE_SUB(NOW(), INTERVAL '.$limit.' HOUR) GROUP BY MONTH(date), DAY(date), HOUR(date)');	
 	$rows4 = array();
 	$rows4['name'] = 'HumiF';
 	while ($donnees = $reponse->fetch())
